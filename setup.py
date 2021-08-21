9d9f28473b0f86ab7a4e165522d3cd124f93a5d0
enjoyintheheat <enjoyintheheat@gmail.com>
2021-08-21 22:12:33 +0300

    Add tests, setup files and json parser skeleton

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+
+setup(name="schematicspy", package_dir={"": "src"})
---
+++ src/schematicspy/parser/json.py
@@ -0,0 +1,14 @@
+from io import TextIOWrapper
+import json
+
+
+class JsonParser:
+    def __init__(self, text: str):
+        self._raw_json = json.loads(text)
+
+    def dump(self) -> str:
+        return json.dumps(self._raw_json)
+
+    def __repr__(self):
+        str_len = len(self._raw_json.encode("utf-8"))
+        return f"<JsonParser [{str_len} Bytes]>"
